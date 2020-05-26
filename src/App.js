8f41794d10d5144fedec28c0bb9fae140ff66900
MarcosRuginski <marcosrobertoruginski@gmail.com>
2020-05-26 19:56:20 -0300

    css and images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 
+import './App.css';
+import backgroundImage from './assets/background.jpg';
+
 import Header from './components/Header';
 
 function App() {
@@ -16,6 +19,7 @@ function App() {
   return (
     <>
       <Header title="Projects" />
+      <img width={300} src={backgroundImage} />
       <ul>
         {projects.map((project, idx) => <li key={idx}>{project}</li>)}
       </ul>
