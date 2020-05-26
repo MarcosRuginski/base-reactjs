cfc3ed420533361ae54e75f47d39b50e0a9d7331
MarcosRuginski <marcosrobertoruginski@gmail.com>
2020-05-26 20:30:43 -0300

    save projects

--- src/App.js
+++ src/App.js
@@ -10,14 +10,20 @@ function App() {
 
   useEffect(() => {
     api.get('projects').then(response => {
-      setProjects(response.data)
+      setProjects(response.data);
     });
   }, []);
 
-  function handleAddPropject() {
-    // projects.push(`Novo projeto ${Date.now()}`);
+  async function handleAddPropject() {
+    // setProjects([...projects, `Novo projeto ${Date.now()}`]);
 
-    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: "Marcos Ruginski"
+    });
+
+    const project = response.data;
+    setProjects([...projects, response.data]);
 
     console.log(projects);
   }
